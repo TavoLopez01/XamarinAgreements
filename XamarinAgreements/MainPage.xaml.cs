2229ea0cec74d850b7d9fcb150e2e545e032932d
Ospina Lopez, Gustavo Adolfo, Enel Colombia Extern <gaospina@indracompany.com>
2021-02-27 17:44:30 -0500

    Agregar archivos de proyecto.

---
+++ XamarinAgreements/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinAgreements
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string api = "https://localhost:44328/Agreements";
+        private readonly HttpClient http = new HttpClient();
+        private ObservableCollection<AgreementsModel> _agreements;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            string query = await http.GetStringAsync(api);
+            List<AgreementsModel> agreementsResult = JsonConvert.DeserializeObject<List<AgreementsModel>>(query);
+            _agreements = new ObservableCollection<AgreementsModel>(agreementsResult);
+            Agreements_List.ItemsSource = _agreements;
+            base.OnAppearing();
+        }
+    }
+}
